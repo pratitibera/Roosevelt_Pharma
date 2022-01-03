178b8e7f583cb4634da773675462d4e06260700d
Pratiti Bera <remabimal0801@gmail.com>
2022-01-04 00:56:13 +0530

    First commit

---
+++ js/main.js
@@ -0,0 +1,94 @@
+// for animation of bar and cross in mobile view
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
+
+jQuery("#backtotop").click(function () {
+    jQuery("body,html").animate({
+        scrollTop: 0
+    }, 600);
+});
+jQuery(window).scroll(function () {
+    if (jQuery(window).scrollTop() > 150) {
+        jQuery("#backtotop").addClass("visible");
+    } else {
+        jQuery("#backtotop").removeClass("visible");
+    }
+});
+
+$(document).ready(function () {
+    // AOS Instance
+    AOS.init();
+
+    $('#blogsSection').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoPlaySpeed: 1000,
+        autoplayHoverPause: true,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: {
+            0: {
+                items: 1
+            },
+            320: {
+                items: 1
+            },
+            560: {
+                items: 2
+            },
+            960: {
+                items: 3
+            }
+        }
+    });
+
+    $('#productSection').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoPlaySpeed: 1000,
+        autoplayHoverPause: true,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation-products .owl-nav-prev'), $('.owl-navigation-products .owl-nav-next')],
+        responsive: {
+            0: {
+                items: 1
+            },
+            320: {
+                items: 1
+            },
+            560: {
+                items: 2
+            },
+            960: {
+                items: 6
+            }
+        }
+    });
+});
+
+$(".navbar-nav > li").click(function () {
+    // To collapse navbar in mobile view when some link item is selected / clicked
+    $(".navbar-nav > li > a").removeClass('active');
+    $(this).children('a').addClass('active');
+    $(".collapse").collapse('hide');
+
+    // To handle the bar and cross icon at the same time
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
